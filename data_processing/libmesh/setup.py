a702bed4de26548762c5ddadb1215f2e98a55211
wzhqwq <wzhii345@outlook.com>
2024-03-07 21:41:37 +0800

    Fix setup problem with libmesh

--- data_processing/libmesh/setup.py
+++ data_processing/libmesh/setup.py
@@ -1,5 +1,13 @@
-from setuptools import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
+import numpy as np
 
 setup(name = 'libmesh',
-      ext_modules = cythonize("*.pyx"))
+      ext_modules = cythonize(
+          Extension(
+              "triangle_hash",
+              sources=["triangle_hash.pyx"],
+              include_dirs=[np.get_include()]
+          )
+      ),
+      install_requires = ['numpy'])
